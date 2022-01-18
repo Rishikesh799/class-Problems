5114474acaddd2ba9595bceccf200e0854d0c29f
Rishikesh799 <97303907+Rishikesh799@users.noreply.github.com>
2022-01-18 21:37:42 +0530

    Update rectangle.py
    Creating reactangle class and their method 
    1)Perimeter 
    2)Area
    3)display
    4)Parallelepipede

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,8 @@
 """
-### Write rectangle class in python language allowing you to build a rectangle with length ,width and height.
+QQQ==Write a Rectangle class in Python language, allowing you to build a rectangle with length and width attributes.
+     Create a Perimeter() method to calculate the perimeter of the rectangle and a Area() method to calculate the area of ​​the rectangle.
+     Create a method display() that display the length, width, perimeter and area of an object created using an instantiation on rectangle class.
+     Create a Parallelepipede child class inheriting from the Rectangle class and with a height attribute and another Volume() method to calculate the volume of the Parallelepiped.
 
 class Rect:
     def __init__(self,l,w):
